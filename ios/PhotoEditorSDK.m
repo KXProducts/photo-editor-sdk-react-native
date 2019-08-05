46e37981ec1fe77c11a6abeed54ba9536401b9a1
Øystein Borg <oysteinborg@ysteins-iMac.local>
2019-08-05 15:41:11 +0200

    Build fixes with new pesdk dependency

--- ios/PhotoEditorSDK.m
+++ ios/PhotoEditorSDK.m
@@ -205,7 +205,7 @@ static NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY
             }
             
             // TODO: Video recording not supported currently
-            b.allowedRecordingModesAsNSNumbers = @[[NSNumber numberWithInteger:RecordingModePhoto]];
+            // b.allowedRecordingModesAsNSNumbers = @[[NSNumber numberWithInteger:RecordingModePhoto]];
         }];
     }];
     
